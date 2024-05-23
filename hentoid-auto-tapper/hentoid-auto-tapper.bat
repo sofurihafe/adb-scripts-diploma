1e3fa79338d32172bb947821ce193a9d39c76356
Andrey Tarbeev <54685068+sofurihafe@users.noreply.github.com>
2024-05-24 00:45:06 +0300

    Edit hentoid adb script

--- hentoid-auto-tapper/hentoid-auto-tapper.bat
+++ hentoid-auto-tapper/hentoid-auto-tapper.bat
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 FOR /L %%A IN (1, 1, 100000) DO (
 	echo ==============%%A==============
 	echo !time!
-	adb shell "input tap 225 78 ; input text %%A ; input keyevent 66 ; input tap 225 78 ; input keyevent 67"
+	adb shell "input tap 225 78 ; input keyevent 66 ; input tap 225 78 ; input keyevent 67"
 	CSCRIPT MILLISLEEP.VBS //Nologo 1000
 	echo ===============================
 	echo:
